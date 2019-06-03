adbdedfd557b24a015b24c4ab6bcc6418d0e25d1
Srinivasan <ssrinivas207@dxc.com>
2019-06-03 13:07:26 -0500

    basic API developement

--- API_development/apps.py
+++ API_development/apps.py
@@ -2,3 +2,19 @@ from flask import Flask,jsonify
 import random
 
 from quotes import funny_quotes
+
+app = Flask(__name__)
+
+@app.route("/api/randquo")
+def random_run_method():
+    quotes = funny_quotes()
+    length = len(quotes)
+    selected_quotes = quotes[random.randint(0,length-1)]
+    return jsonify(selected_quotes)
+
+#val = random_run_method()
+#print(val)
+
+if __name__ == "__main__":
+    app.run()
+
