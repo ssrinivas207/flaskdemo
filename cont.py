f988acc76b88c22d375cac5bb30f1f13bc942881
Srinivasan <ssrinivas207@dxc.com>
2019-06-04 10:26:10 -0500

    learnings on CLI

--- cont.py
+++ cont.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("main.html")
+    return 'Hello DXC'
+    #return render_template("main.html")
+
+@app.route('/user/<name>')
+def user(name):
+    return 'Hi, %s' %format(name)
 
 if __name__ == "__main__":
     app.run()
